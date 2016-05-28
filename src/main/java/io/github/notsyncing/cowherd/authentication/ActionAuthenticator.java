8e1aaf061fe1bbb4833c912b7d229865c05f4905
0xfc <song.fc@gmail.com>
2016-05-28 11:41:54 +0800

    补充验证器接口的注释。

--- src/main/java/io/github/notsyncing/cowherd/authentication/ActionAuthenticator.java
+++ src/main/java/io/github/notsyncing/cowherd/authentication/ActionAuthenticator.java
@@ -5,7 +5,17 @@ import io.github.notsyncing.cowherd.models.FilterContext;
 import java.lang.annotation.Annotation;
 import java.util.concurrent.CompletableFuture;
 
+/**
+ * 服务请求异步验证器接口
+ * @param <A> 该验证器所对应的注解类
+ */
 public interface ActionAuthenticator<A extends Annotation>
 {
+    /**
+     * 异步验证方法
+     * @param authAnnotation 该验证器所对应的注解类实例
+     * @param context 过滤器上下文对象
+     * @return 指示请求是否验证通过的 CompletableFuture 对象
+     */
     CompletableFuture<Boolean> authenticate(A authAnnotation, FilterContext context);
 }
