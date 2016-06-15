92ed211976906493b9b404bf5c508fea3b9bbd7e
0xfc <song.fc@gmail.com>
2016-06-15 17:21:11 +0800

    core: 为Pair类增加一些工具方法。

--- cowherd-core/src/main/java/io/github/notsyncing/cowherd/models/Pair.java
+++ cowherd-core/src/main/java/io/github/notsyncing/cowherd/models/Pair.java
@@ -1,5 +1,11 @@
 package io.github.notsyncing.cowherd.models;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentMap;
+import java.util.stream.Collectors;
+
 public class Pair<K, V>
 {
     private K key;
@@ -33,5 +39,43 @@ public class Pair<K, V>
     public void setValue(V value)
     {
         this.value = value;
+    }
+
+    public static <K, V> boolean listContainsKey(List<Pair<K, V>> list, K key)
+    {
+        return list.stream().anyMatch(p -> p.getKey().equals(key));
+    }
+
+    public static <K, V> V listGetValue(List<Pair<K, V>> list, K key)
+    {
+        return list.stream().filter(p -> p.getKey().equals(key)).map(Pair::getValue).findFirst().orElse(null);
+    }
+
+    public static <K, V> List<V> listGetValues(List<Pair<K, V>> list, K key)
+    {
+        return list.stream().filter(p -> p.getKey().equals(key)).map(Pair::getValue).collect(Collectors.toList());
+    }
+
+    public static <K, V> V listSetValue(List<Pair<K, V>> list, K key, V value)
+    {
+        Pair<K, V> pair = list.stream().filter(p -> p.getKey().equals(key)).findFirst().orElse(null);
+
+        if (pair == null) {
+            return null;
+        }
+
+        V oldValue = pair.getValue();
+        pair.setValue(value);
+        return oldValue;
+    }
+
+    public static <K, V> Map<K, V> listToMap(List<Pair<K, V>> list)
+    {
+        return list.stream().collect(Collectors.toMap(Pair::getKey, Pair::getValue));
+    }
+
+    public static <K, V> ConcurrentMap<K, V> listToConcurrentMap(List<Pair<K, V>> list)
+    {
+        return list.stream().collect(Collectors.toConcurrentMap(Pair::getKey, Pair::getValue));
     }
 }
