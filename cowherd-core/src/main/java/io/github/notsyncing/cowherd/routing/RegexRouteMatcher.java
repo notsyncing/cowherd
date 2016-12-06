0721ceeb4aade0aee9e16eaa4fea649e6ee64a9f
notsyncing <song.fc@gmail.com>
2016-12-06 17:53:17 +0800

    core: 重构正则路由匹配的部分代码。

--- cowherd-core/src/main/java/io/github/notsyncing/cowherd/routing/RegexRouteMatcher.java
+++ cowherd-core/src/main/java/io/github/notsyncing/cowherd/routing/RegexRouteMatcher.java
@@ -1,9 +1,13 @@
 package io.github.notsyncing.cowherd.routing;
 
+import io.github.notsyncing.cowherd.models.Pair;
 import io.github.notsyncing.cowherd.models.RouteInfo;
-import io.github.notsyncing.cowherd.utils.RouteUtils;
+import io.github.notsyncing.cowherd.utils.RegexUtils;
+import io.github.notsyncing.cowherd.utils.StringUtils;
 
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RegexRouteMatcher extends RouteMatcher
 {
@@ -12,9 +16,53 @@ public class RegexRouteMatcher extends RouteMatcher
         super(uri);
     }
 
+    private MatchedRoute matchRoute(URI uri, RouteInfo info, boolean matchOnly)
+    {
+        if (("/".equals(uri.getPath())) && (info.isEntry())) {
+            return new MatchedRoute();
+        }
+
+        if ((info.getDomainPattern() != null) && (!info.getDomainPattern().matcher(uri.getHost()).find())) {
+            return null;
+        }
+
+        if (info.getPathPattern() == null) {
+            return null;
+        }
+
+        if (info.getPathPattern().matcher(StringUtils.stripSameCharAtStringHeader(uri.getPath(), '/')).find()) {
+            if (matchOnly) {
+                return new MatchedRoute();
+            }
+
+            MatchedRoute route = new MatchedRoute(extractRouteParameters(uri, info));
+            route.setRoute(info);
+
+            return route;
+        }
+
+        return null;
+    }
+
+    private List<Pair<String, String>> extractRouteParameters(URI uri, RouteInfo route)
+    {
+        List<Pair<String, String>> params = new ArrayList<>();
+
+        if (route.getDomainPattern() != null) {
+            RegexUtils.addMatchedGroupsToPairList(uri.getHost(), route.getDomainPattern(), params);
+        }
+
+        if (route.getPathPattern() != null) {
+            RegexUtils.addMatchedGroupsToPairList(StringUtils.stripSameCharAtStringHeader(uri.getPath(), '/'),
+                    route.getPathPattern(), params);
+        }
+
+        return params;
+    }
+
     @Override
     protected MatchedRoute match(RouteInfo route, boolean matchOnly)
     {
-        return RouteUtils.matchRoute(uri, route, matchOnly);
+        return matchRoute(uri, route, matchOnly);
     }
 }
--- cowherd-core/src/main/java/io/github/notsyncing/cowherd/utils/RouteUtils.java
+++ cowherd-core/src/main/java/io/github/notsyncing/cowherd/utils/RouteUtils.java
@@ -1,64 +1,15 @@
 package io.github.notsyncing.cowherd.utils;
 
 import io.github.notsyncing.cowherd.annotations.httpmethods.*;
-import io.github.notsyncing.cowherd.models.Pair;
-import io.github.notsyncing.cowherd.models.RouteInfo;
-import io.github.notsyncing.cowherd.routing.MatchedRoute;
 import io.vertx.core.http.HttpMethod;
 import io.vertx.core.http.HttpServerRequest;
 
 import java.lang.reflect.Method;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class RouteUtils
 {
-    public static MatchedRoute matchRoute(URI uri, RouteInfo info, boolean matchOnly)
-    {
-        if (("/".equals(uri.getPath())) && (info.isEntry())) {
-            return new MatchedRoute();
-        }
-
-        if ((info.getDomainPattern() != null) && (!info.getDomainPattern().matcher(uri.getHost()).find())) {
-            return null;
-        }
-
-        if (info.getPathPattern() == null) {
-            return null;
-        }
-
-        if (info.getPathPattern().matcher(StringUtils.stripSameCharAtStringHeader(uri.getPath(), '/')).find()) {
-            if (matchOnly) {
-                return new MatchedRoute();
-            }
-
-            MatchedRoute route = new MatchedRoute(extractRouteParameters(uri, info));
-            route.setRoute(info);
-
-            return route;
-        }
-
-        return null;
-    }
-
-    public static List<Pair<String, String>> extractRouteParameters(URI uri, RouteInfo route)
-    {
-        List<Pair<String, String>> params = new ArrayList<>();
-
-        if (route.getDomainPattern() != null) {
-            RegexUtils.addMatchedGroupsToPairList(uri.getHost(), route.getDomainPattern(), params);
-        }
-
-        if (route.getPathPattern() != null) {
-            RegexUtils.addMatchedGroupsToPairList(StringUtils.stripSameCharAtStringHeader(uri.getPath(), '/'),
-                    route.getPathPattern(), params);
-        }
-
-        return params;
-    }
-
     private static String httpMethodToString(HttpMethod m)
     {
         switch (m) {
